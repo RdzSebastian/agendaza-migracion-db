e3e8ee722a59612888954159d5736c568818f1cf
Lorenzomar4 <m100_98@hotmail.com.ar>
2024-04-07 14:29:49 -0300

    pequeños cambios

--- migracion/Conexión.py
+++ migracion/Conexión.py
@@ -4,6 +4,7 @@ import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine, exc
 
+
 # Parámetros de conexión a la base de datos
 agendaza_db_parametros = {
     'dbname': 'agendaza',
@@ -40,29 +41,33 @@ class ConexionBD:
         try:
             url = self.construir_url()
             self.engine = create_engine(url)
+          
             print(f"conexion para la base {self.db_parametros['dbname']} fue realizada correctamente")
         except exc.SQLAlchemyError as e:
             print("Error al conectar a la base de datos:", e)
 
 
-    def probar_consulta(self, query):
+    def realizar_consulta(self, query):
         if self.engine is None:
             self.realizar_conexion()
         try:
             resultado = pd.read_sql_query(query, self.engine)
-            print("Resultado de la consulta:")
-            print(resultado)
+            return resultado
         except exc.SQLAlchemyError as e:
             print("Error al ejecutar la consulta:", e)
+
+    def visualizar_consulta(self, query):
+        print(self.realizar_consulta(query))
+
 
 
 
 conexion_agendaza = ConexionBD(agendaza_db_parametros)
-conexion_agendaza.probar_consulta('select * from usuario')
+conexion_agendaza.visualizar_consulta('select * from usuario')
 
 
 conexion_geservapp = ConexionBD(geservapp_db_parametros)
-conexion_geservapp.probar_consulta('select * from usuario')
+conexion_geservapp.visualizar_consulta('select * from usuario')
 
 
 
